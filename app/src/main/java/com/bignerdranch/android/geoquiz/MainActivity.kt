09193551474fd58ef26b01c530148aa5b05c928e
rane20 <145800950+rane20@users.noreply.github.com>
2024-09-08 18:37:29 -0700

    beginning of ch.3 changes

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.bignerdranch.android.geoquiz
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,6 +10,10 @@ import android.widget.Button
 
 import com.bignerdranch.android.geoquiz.databinding.ActivityMainBinding
 import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+
 
 private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
@@ -15,17 +21,18 @@ class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var binding: ActivityMainBinding
+    private var correctAnswersCount = 0
+    private val quizViewModel: QuizViewModel by viewModels()
 
-    private val questionBank = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true)
+    private val cheatLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){ result ->
+       if (result.resultCode == Activity.RESULT_OK){
+           quizViewModel.isCheater =
+               result.data?.getBooleanExtra(EXTRA_ANSWER_SHOWN, false) ?: false
+       }
+    }
 
-    )
-    private var currentIndex = 0
 
     @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,6 +43,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        Log.d(TAG, "Got a QuizVIewModel: $quizViewModel")
+
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
 
@@ -43,27 +52,30 @@ class MainActivity : AppCompatActivity() {
 
         binding.trueButton.setOnClickListener { view: View ->
             checkAnswer(true)
-
+            disableAnswerButtons()
+            checkIfQuizCompleted()
         }
 
         binding.falseButton.setOnClickListener { view: View ->
             checkAnswer(false)
+            disableAnswerButtons()
+            checkIfQuizCompleted()
         }
 
         binding.nextButton.setOnClickListener{
-            currentIndex = (currentIndex + 1) % questionBank.size
-            val questionTextResId = questionBank[currentIndex].textResId
-            binding.questionTextView.setText(questionTextResId)
+           quizViewModel.moveToNext()
             updateQuestion()
         }
         binding.prevButton.setOnClickListener{
-            currentIndex = (currentIndex - 1) % questionBank.size
-            val questionTextResId = questionBank[currentIndex].textResId
-            binding.questionTextView.setText(questionTextResId)
+            quizViewModel.moveToPrev()
             updateQuestion()
         }
-        val questionTextResId = questionBank[currentIndex].textResId
-        binding.questionTextView.setText(questionTextResId)
+        binding.cheatButton.setOnClickListener{
+            val answerIsTrue = quizViewModel.currentQuestionAnswer
+            val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
+            startActivity(intent)
+            cheatLauncher.launch(intent)
+        }
         updateQuestion()
 
     }
@@ -88,23 +100,47 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "onStart() called")
     }
     private fun updateQuestion(){
-        val questionTextResId = questionBank[currentIndex].textResId
+        val questionTextResId = quizViewModel.currentQuestionText
         binding.questionTextView.setText(questionTextResId)
+        enableAnswerButtons()
+    }
+    private fun disableAnswerButtons() {
+        binding.trueButton.isEnabled = false
+        binding.falseButton.isEnabled = false
+    }
+    private fun enableAnswerButtons() {
+        binding.trueButton.isEnabled = true
+        binding.falseButton.isEnabled = true
     }
     private fun checkAnswer(userAnswer: Boolean){
-        val correctAnswer = questionBank[currentIndex].answer
+        val correctAnswer = quizViewModel.currentQuestionAnswer
+        val messageResId = when {
+            quizViewModel.isCheater -> R.string.judgement_toast
+            userAnswer == correctAnswer -> {
+                correctAnswersCount++
+                R.string.correct_toast
+            }
+                else -> R.string.incorrect_toast
+            }
 
-        val messageResId = if(userAnswer == correctAnswer) {
-            R.string.correct_toast
-        }else{
-            R.string.incorrect_toast
-        }
         Snackbar.make(
             binding.root,  // Use the root view of your layout
             messageResId,  // The message to display
             Snackbar.LENGTH_SHORT // Duration of the Snackbar
         ).show()
 
+    }
+
+    private fun checkIfQuizCompleted() {
+        if (quizViewModel.ifCompleted) { // Use the computed property from ViewModel
+            displayScore()
+        }
+    }
+    private fun displayScore() {
+        val scorePercentage = (correctAnswersCount.toDouble() / quizViewModel.getQuestionBank().size) * 100
+        val scoreMessage = getString(R.string.quiz_score, scorePercentage.toInt())
+
+        Toast.makeText(this, scoreMessage, Toast.LENGTH_LONG).show()
     }
 }
 
